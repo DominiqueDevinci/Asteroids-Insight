14bd0e39e529eddf48e4909b946abb5ae0117a00
DominiqueDevinci <dominique.blaze@devinci.fr>
2018-01-11 23:11:35 +0100

    reducer: receive closest asteroids

---
+++ src/Actions.js
@@ -0,0 +1,15 @@
+
+
+/* action creators */
+
+
+// receive asteroids from rest api, for instance
+// ex: https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-09-03&end_date=2017-09-08&api_key=DEMO_KEY
+export const RECEIVE_CLOSEST_ASTEROIDS = 'RECEIVE_CLOSEST_ASTEROIDS'
+
+function receiveAsteroids(asteroids){
+    return {
+        type: RECEIVE_CLOSEST_ASTEROIDS,
+        asteroids
+    }
+}
---
+++ src/reducers/tables.js
@@ -0,0 +1,22 @@
+// here all reducers wich deals with datas wich will be displayed in tables
+
+
+// state.tables.closest : list of closest asteroids between two given dates
+function closest(
+    state={ // initial state
+        length: 0,
+        asteroids: {}
+    }, action
+){
+    switch(action.type){
+        case RECEIVE_CLOSEST_ASTEROIDS:
+            // receive this kind of data in action.asteroids
+            // ex: https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-09-03&end_date=2017-09-08&api_key=DEMO_KEY
+            return {
+                length: action.asteroids.element_count,
+                asteroids: action.asteroids.near_earth_objects // asteroids indexed by date of closest approach
+            }
+        default:
+            return state;
+    }
+}
